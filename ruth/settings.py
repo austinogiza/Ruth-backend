2e12a8e4146f720db61f5329d960f884660f2f39
Augustine Ogiza <austinogiza@gmail.com>
2024-03-17 11:16:49 +0100

    db update

--- ruth/settings.py
+++ ruth/settings.py
@@ -103,7 +103,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'ruth',
         'USER': 'ruth',
-        'PASSWORD': 'password',
+        'PASSWORD': 'ruth',
         'HOST': 'localhost',
         'PORT': '5432',
     }
