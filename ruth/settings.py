796578ab83e9b7098fb77107035ee5c9efa2ba52
Augustine Ogiza <austinogiza@gmail.com>
2024-05-20 03:17:34 +0100

    static files path

--- ruth/settings.py
+++ ruth/settings.py
@@ -144,28 +144,28 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 ##developments
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [
+# STATIC_URL = '/static/'
+# STATICFILES_DIRS = [
 
-    # os.path.join(BASE_DIR, 'static')
+#     # os.path.join(BASE_DIR, 'static')
 
-]
-STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# ]
+# STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
+# MEDIA_URL = '/media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 
 #production
-# STATIC_URL = '/static/'
+STATIC_URL = '/static/'
 
-# STATICFILES_DIRS = [
-#   os.path.join(BASE_DIR, 'assets')
-# ]
+STATICFILES_DIRS = [
+  os.path.join(BASE_DIR, 'assets')
+]
 
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-# MEDIA_URL = '/media/'
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 
