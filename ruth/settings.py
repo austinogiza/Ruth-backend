760c30c1a86b6b56ef20550d9066f63d3efe430d
Augustine Ogiza <austinogiza@gmail.com>
2024-05-23 22:01:57 +0100

    debug env format

--- ruth/settings.py
+++ ruth/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
@@ -94,10 +94,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'HOST':  "localhost",
-        'NAME': os.getenv('DB_NAME'),
-        'USER':  os.getenv('DB_USER'),
+        'NAME': os.environ.get('DB_NAME'),
+        'USER':  os.environ.get('DB_USER'),
         'PORT': '5432',
-        'PASSWORD':  os.getenv('DB_PASSWORD'),
+        'PASSWORD':  os.environ.get('DB_PASSWORD'),
     }
 }
 
@@ -186,7 +186,7 @@ ACCOUNT_UNIQUE_EMAIL = True
 EMAIL_BACKEND = "anymail.backends.resend.EmailBackend"
 ANYMAIL = {
 
-    "RESEND_API_KEY":  os.getenv('RESEND_API_KEY'),
+    "RESEND_API_KEY":  os.environ.get('RESEND_API_KEY'),
 }
 # EMAIL_USE_SSL = False
 #
